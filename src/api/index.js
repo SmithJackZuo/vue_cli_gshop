58775ed9a95ec78bdc4d938dd954739c6d018801
SmithJackZuo <2546078657@qq.com>
2019-11-26 17:35:36 +0800

    4848

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
 /* 封装ajax函数 */
 import ajax from './ajax'
 
-const baseUrl = '/sit_' //sit环境
-  // const baseUrl = '/uatc_' //uatc环境
-  // const baseUrl = '/' // 生产环境(uata)
-  // const baseUrl = '/api/sit_' //开发环境(设置代理)
+// const baseUrl = '/sit_' //sit环境
+// const baseUrl = '/uat_' //uat环境
+// const baseUrl = '/' // 生产环境(uata)
+const baseUrl = '/api/sit_' //开发环境(设置代理)
 export const moneyManageList = (data) => ajax(baseUrl + 'safe/finProductInfoQry.do', data, 'POST')
 export const depositManageList = (data) => ajax(baseUrl + 'lsDeposit/depositProdInfo.do', data, 'POST')
